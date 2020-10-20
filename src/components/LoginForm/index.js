60ec516e7fd52f00e0fab5f01f6237d67f9ae5bf
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-10-21 00:38:37 +0200

    Add validation to LoginForm

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,13 +1,39 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import PropTypes from 'prop-types'
+
+import isEmail from 'validator/lib/isEmail'
+
 import Input from '../Input'
 import Button from '../Button'
+import { INVALID_EMAIL_ERR, REQUIRED_FIELD_ERR, MIN_PASSWORD_LENGTH, MIN_PASSWORD_LENGTH_ERR } from '../../constants'
+
+const validateLoginForm = ({ email, password }) => {
+    const validationErrors = { email: [], password: [] }
+    if (email.trim() === '') validationErrors.email.push(REQUIRED_FIELD_ERR('Email'))
+    else if (!isEmail(email)) validationErrors.email.push(INVALID_EMAIL_ERR)
+
+    if (password.trim() === '') validationErrors.password.push(REQUIRED_FIELD_ERR('Password'))
+    else if (password.length < MIN_PASSWORD_LENGTH) validationErrors.password.push(MIN_PASSWORD_LENGTH_ERR)
+
+    return validationErrors
+}
 
 export default function LoginForm({ onSubmit }) {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    
-    return <form onSubmit={onSubmit}>
+
+    const handleFormSubmit = useCallback(
+        (e) => {
+            e.preventDefault()
+            const validationResult = validateLoginForm({ email, password })
+            if (!validationResult.email.length && !validationResult.password.length)
+                onSubmit({ email, password })
+            else console.error(validationResult)
+        },
+        [onSubmit, email, password]
+    )
+
+    return <form onSubmit={handleFormSubmit}>
         <Input
             id='email'
             type='email'
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,5 @@
 export const api = 'https://private-b2e6827-robustatask.apiary-mock.com'
+export const INVALID_EMAIL_ERR = 'Email is invalid'
+export const REQUIRED_FIELD_ERR = fieldName => `${fieldName} is Required`
+export const MIN_PASSWORD_LENGTH_ERR = 'Password is too short'
+export const MIN_PASSWORD_LENGTH = 6
