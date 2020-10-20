c450a419798f31cb48ff6130182cd3dc1a540e31
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-10-20 22:56:05 +0200

    Set document title in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AuthForm } from './components'
 
 import classes from './App.module.css'
 
 function App() {
+  useEffect(() => {
+    document.title = 'Sign up | Robusta'
+  }, [])
+  
   return <main>
     <AuthForm className={classes.authForm} />
   </main>
