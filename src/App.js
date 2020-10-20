8fc07244d5320d22c1371ae6c7ab9ab679a1aa84
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-10-20 06:41:44 +0200

    Insert task HTML in App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import React from 'react'
 function App() {
   return (
     <div>
-      Hello
+      <form action="https://private-b2e6827-robustatask.apiary-mock.com/auth/register" method="post">
+        <div className="form-group">
+          <span>Full Name</span>
+          <input type="text" id="username" />
+        </div>
+        <div className="form-group">
+          <span>Email</span>
+          <input type="text" id="username" />
+        </div>
+        <div className="form-group">
+          <span>Username</span>
+          <input type="text" id="username" />
+        </div>
+        <div className="form-group">
+          <span>Password</span>
+          <input type="password" id="password" />
+        </div>
+        <input type="submit" value="submit" />
+      </form>
     </div>
   )
 }
