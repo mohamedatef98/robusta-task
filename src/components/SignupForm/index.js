87172bed081dfd85ffeb04e3f1ed089b681bbcff
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-10-21 00:53:10 +0200

    Add validation to SignupForm

--- src/components/SignupForm/index.js
+++ src/components/SignupForm/index.js
@@ -1,15 +1,50 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import PropTypes from 'prop-types'
+import isEmail from 'validator/lib/isEmail'
+
 import Input from '../Input'
 import Button from '../Button'
+import { INVALID_EMAIL_ERR, MIN_PASSWORD_LENGTH, MIN_PASSWORD_LENGTH_ERR, REQUIRED_FIELD_ERR } from '../../constants'
+
+const validateSignupForm = ({ fullName, userName, email, password }) => {
+    const validationErrors = { fullName: [], userName: [], email: [], password: [] }
+
+    if (fullName.trim() === '') validationErrors.fullName.push(REQUIRED_FIELD_ERR('Full Name'))
+
+    if (userName.trim() === '') validationErrors.userName.push(REQUIRED_FIELD_ERR('Username'))
+
+    if (email.trim() === '') validationErrors.email.push(REQUIRED_FIELD_ERR('Email'))
+    else if (!isEmail(email)) validationErrors.email.push(INVALID_EMAIL_ERR)
+
+    if (password.trim() === '') validationErrors.password.push(REQUIRED_FIELD_ERR('Password'))
+    else if (password.length < MIN_PASSWORD_LENGTH) validationErrors.password.push(MIN_PASSWORD_LENGTH_ERR)
+
+    return validationErrors
+}
 
 export default function SignupForm({ onSubmit }) {
     const [fullName, setFullName] = useState('')
     const [email, setEmail] = useState('')
     const [userName, setUserName] = useState('')
     const [password, setPassword] = useState('')
-    
-    return <form onSubmit={onSubmit}>
+    const [formErrors, setFormErrors] = useState({})
+
+    const handleFormSubmit = useCallback(
+        (e) => {
+            e.preventDefault()
+            const validationResults = validateSignupForm({ fullName, userName, email, password })
+            if (
+                !validationResults.fullName.length &&
+                !validationResults.userName.length &&
+                !validationResults.email.length &&
+                !validationResults.password.length
+            ) onSubmit({ fullName, userName, email, password })
+            else setFormErrors(validationResults)
+        },
+        [fullName, userName, email, password, onSubmit]
+    )
+
+    return <form onSubmit={handleFormSubmit}>
         <Input
             id='fullname'
             type='text'
@@ -18,6 +53,7 @@ export default function SignupForm({ onSubmit }) {
             name='fullname'
             value={fullName}
             onChange={e => setFullName(e.target.value)}
+            error={formErrors.fullName?.[0]}
         />
         <Input
             id='email'
@@ -27,6 +63,7 @@ export default function SignupForm({ onSubmit }) {
             name='email'
             value={email}
             onChange={e => setEmail(e.target.value)}
+            error={formErrors.email?.[0]}
         />
         <Input
             id='username'
@@ -36,6 +73,7 @@ export default function SignupForm({ onSubmit }) {
             name='username'
             value={userName}
             onChange={e => setUserName(e.target.value)}
+            error={formErrors.userName?.[0]}
         />
         <Input
             id='password'
@@ -45,6 +83,7 @@ export default function SignupForm({ onSubmit }) {
             name='password'
             value={password}
             onChange={e => setPassword(e.target.value)}
+            error={formErrors.password?.[0]}
         />
         <Button type='submit'>
             Signup
