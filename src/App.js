5468e8ebbffbcc82671d2d19a4499cb9757b0bed
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-10-21 03:46:04 +0200

    Format App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { AuthForm } from './components'
 import classes from './App.module.css'
 
 function App() {
-  useEffect(() => {
-    document.title = 'Sign up | Robusta'
-  }, [])
-  
-  return <main>
-    <AuthForm className={classes.authForm} />
-  </main>
+    useEffect(() => {
+        document.title = 'Sign up | Robusta'
+    }, [])
+
+    return <main>
+        <AuthForm
+            className={classes.authForm}
+        />
+    </main>
 }
 
 export default App
